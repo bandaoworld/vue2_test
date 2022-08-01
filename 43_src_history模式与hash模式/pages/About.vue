c10aa5c0d30e96c4a431efb3fef5b80b543df5c8
bandaoworld <86190021+bandaoworld@users.noreply.github.com>
2022-08-02 01:32:44 +0800

    Add files via upload

---
+++ 43_src_history模式与hash模式/pages/About.vue
@@ -0,0 +1,25 @@
+<template>
+    <h2>我是About的内容</h2>
+</template>
+
+<script>
+export default {
+    name: 'myAbout',
+    beforeRouteEnter(to, from, next) {
+        console.log('About--beforeRouteEnter', to, from)
+        if(to.meta.isAuth) {//判断是否需要鉴权
+            if (localStorage.getItem('school') === 'xupt') {
+                next()
+            } else {
+                alert('学校名不是xupt，无权限查看！')
+            }
+        } else {
+            next()
+        }
+    },
+    beforeRouteLeave(to, from, next){
+        console.log('About--beforeRouteLeave',to,from)
+		next()
+    }
+}
+</script>
