3c8298cccec55cb3b54f355a3fb9c1f9cd27ea37
bandaoworld <86190021+bandaoworld@users.noreply.github.com>
2022-08-02 01:34:08 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# vue2_test
-2022年5月
+# vue_test
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <button>这是一个按钮</button>
+        <bandao-row>
+            <bandao-button>默认按钮</bandao-button>
+            <bandao-button type="primary">主要按钮</bandao-button>
+            <bandao-button type="success">成功按钮</bandao-button>
+            <bandao-button type="info">信息按钮</bandao-button>
+            <bandao-button type="warning">警告按钮</bandao-button>
+            <bandao-button type="danger">危险按钮</bandao-button>
+        </bandao-row>
+        <bandao-row>
+            <bandao-button icon="bandao-icon-search" circle></bandao-button>
+            <bandao-button type="primary" icon="bandao-icon-edit" circle></bandao-button>
+            <bandao-button type="success" icon="bandao-icon-check" circle></bandao-button>
+            <bandao-button type="info" icon="bandao-icon-message" circle></bandao-button>
+            <bandao-button type="warning" icon="bandao-icon-star-off" circle></bandao-button>
+            <bandao-button type="danger" icon="bandao-icon-dbandaoete" circle></bandao-button>
+        </bandao-row>
+        <bandao-date-picker 
+            type="date" 
+            placeholder="选择日期">
+        </bandao-date-picker>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+    data() {
+        return {
+            pickerOptions: {
+                // 禁用时间（返回值为boolean）
+                disabledDate(time) {
+                    // return time.getTime() < Date.now();// 禁用当前时间之前的时间
+                    return time.getTime() > Date.now();// 禁用当前时间之后的时间
+                }
+            },
+            addForm: {
+                sbandaoectDates: ''
+            }
+        }
+    }
+}
+
+</script>
+
+<style>
+* {
+    padding: 5px;
+}
+</style>
